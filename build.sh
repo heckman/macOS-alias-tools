8c97626a44ab2ff08d8e65794c9e6b45ba5a30c0
Erik Ben Heckman <erik@heckman.ca>
2026-05-10 14:42:21 -0400

    On build, copy binaries to ./bin

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -e
 cd ${0:h}
 
 print "Cleaning old build files..."
-rm -rf .build
+rm -rf .build bin
 
 VERSION="${${APP_VERSION:-0.0.1}#v}"
 
@@ -18,4 +18,10 @@ then
 	print "\nBuild complete!"
 	print "Location: ${0:A:h}/.build/apple/Products/Release/"
 	ls -lh "${0:A:h}/.build/apple/Products/Release/"{readalias,mkalias}
+	print "Copying binaries to ${0:A:h}/bin/"
+	mkdir -p bin
+	cp \
+		.build/apple/Products/Release/readalias \
+		.build/apple/Products/Release/mkalias \
+		bin
 fi
