89527f3cc52b72cdb668a131a430318ba2fb5743
Erik Ben Heckman <erik@heckman.ca>
2026-05-11 10:52:17 -0400

    Apply the name macOS-alias-utils throughout

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "alias-utils",
+    name: "macOS-alias-utils",
     targets: [
         .target(
             name: "config",
--- Sources/config/package-info.swift
+++ Sources/config/package-info.swift
@@ -1,5 +1,5 @@
 public let about = """
-        (alias-utils v\(versionNumber))
+        (macOS-alias-utils v\(versionNumber))
         Copyright (c) 2026 Erik Ben Heckman
         SPDX-License-Identifier: MIT
         <https://github.com/heckman/macOS-alias-utils/>
